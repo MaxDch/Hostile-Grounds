1450afa5cbaef00460d9b67fa73b1f6b18d3f8ee
jenniiiiiiii11 <mietton.jennifer1@gmail.com>
2024-02-22 15:19:56 +0100

    script modifier

--- Assets/script.cs
+++ Assets/script.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class script : MonoBehaviour
 {
+    public int PV;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PV = 100;
     }
 
     // Update is called once per frame
