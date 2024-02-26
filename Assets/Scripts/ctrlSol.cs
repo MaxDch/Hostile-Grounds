8f05df0e3a3ae79503a6cac91999709dc0f8e9a8
jenniiiiiiii11 <mietton.jennifer1@gmail.com>
2024-02-26 12:27:16 +0100

    PIEGE

--- Assets/Scripts/ctrlSol.cs
+++ Assets/Scripts/ctrlSol.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ctrlSol : MonoBehaviour
 {
+    public GameObject valeurPV;
+    public GameObject zonepiege;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        zonepiege.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        gamemanager.PV = gamemanager.PV - 10;
+        gamemanager.PV = gamemanager.PV - 100;
+        Debug.Log("vie = " + gamemanager.PV.ToString());
+        valeurPV.GetComponent<Text>().text = gamemanager.PV.ToString();
     }
 }
