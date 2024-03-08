6845211ba4b6536313e27f77be2d47f0a7e392c7
MaximeDCH <maximeducharme45@gmail.com>
2024-03-08 13:12:23 +0100

    LifeBar Pompes + import Modé Pompes

--- Assets/Scripts/Ctrl_Pompe_AI.cs
+++ Assets/Scripts/Ctrl_Pompe_AI.cs
@@ -7,23 +7,34 @@ public class Ctrl_Pompe_AI : MonoBehaviour
 {
     public GameObject Minion;
     public GameObject SpawnPoint;
+    public bool _hasAlreadySpawn;
     
     void Start()
     {
-        
+        _hasAlreadySpawn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+          
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player")) 
-        { 
+        if (other.CompareTag("Player") && !_hasAlreadySpawn)
+        {
             Instantiate(Minion, SpawnPoint.transform.position, Quaternion.identity);
+            _hasAlreadySpawn = true;
+
+
+
+        }
         
+
+        else if(other.CompareTag("Player") && _hasAlreadySpawn)
+        {
+            _hasAlreadySpawn = true;
+            return;
         }
     }
 
--- Assets/Scripts/Ctrl_Pompes.cs
+++ Assets/Scripts/Ctrl_Pompes.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ctrl_Pompes : MonoBehaviour
 {
     Health _health;
+    public Slider _healthBar;
     void Start()
     {
         _health = GetComponent<Health>();
         _health._onHealthChanged += OnReceiveDamage;
+        _healthBar.value = 100;
+        
     }
 
     void OnReceiveDamage()
     {
+        _healthBar.value = _health._currentHealth;
+
         //_health._currentHealth 
     }
 }
